38fef4984decdac794488707ac495d966cc4f1e3
R.Haris Nauman <208430036+RoastedKaju@users.noreply.github.com>
2026-06-14 04:27:51 +0500

    Add Manifest files

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,52 @@
+#include <SDL3/SDL.h>
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
+        std::cerr << "Failed to Init Video: " << SDL_GetError() << "\n";
+        return EXIT_FAILURE;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("Hello Window", 800, 600, SDL_WINDOW_RESIZABLE);
+    if (!window)
+    {
+        std::cerr << "Failed to create window: " << SDL_GetError() << "\n";
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, nullptr);
+    if (!renderer)
+    {
+        std::cerr << "Failed to create renderer: " << SDL_GetError() << "\n";
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_EVENT_QUIT)
+            {
+                running = false;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 128, 100, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return EXIT_SUCCESS;
 }
