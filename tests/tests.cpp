211736102f59beaa4c854a6bd8bda766a360de38
R.Haris Nauman <208430036+RoastedKaju@users.noreply.github.com>
2026-06-23 09:07:21 +0500

    Update tests.cpp

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -5,6 +5,7 @@
 #include "application.hpp"
 #include "vulkan_context.hpp"
 #include "renderer.hpp"
+#include "swapchain.hpp"
 
 TEST(Application, Application_Creation_ReturnsValidInstance)
 {
@@ -68,4 +69,35 @@ TEST(Context, VulkanContext_Allocator_IsValidHandle)
     std::unique_ptr<VulkanContext> ctx = std::make_unique<VulkanContext>(config);
 
     EXPECT_NE(ctx->getAllocator(), VK_NULL_HANDLE);
+}
+
+TEST(Renderer, Renderer_ImageIndex_IsZero)
+{
+    Config config{.enableValidation = true, .validationLayers{"VK_LAYER_KHRONOS_validation"}, .deviceIndex = 0};
+    std::unique_ptr<VulkanContext> ctx = std::make_unique<VulkanContext>(config);
+    ctx->createWindow("Test-Window", 1, 1);
+    std::unique_ptr<Renderer> renderer = std::make_unique<Renderer>(*ctx);
+
+    EXPECT_EQ(renderer->getImageIndex(), 0);
+}
+
+TEST(Swapchain, Renderer_Swapchain_IsValid)
+{
+    Config config{.enableValidation = true, .validationLayers{"VK_LAYER_KHRONOS_validation"}, .deviceIndex = 0};
+    std::unique_ptr<VulkanContext> ctx = std::make_unique<VulkanContext>(config);
+    ctx->createWindow("Test-Window", 1, 1);
+    std::unique_ptr<Renderer> renderer = std::make_unique<Renderer>(*ctx);
+
+    EXPECT_NE(renderer->getSwapchain().getSwapchain(), VK_NULL_HANDLE);
+}
+
+
+TEST(Swapchain, Swapchain_Format_B8G8R8A8_SRGB)
+{
+    Config config{.enableValidation = true, .validationLayers{"VK_LAYER_KHRONOS_validation"}, .deviceIndex = 0};
+    std::unique_ptr<VulkanContext> ctx = std::make_unique<VulkanContext>(config);
+    ctx->createWindow("Test-Window", 1, 1);
+    std::unique_ptr<Renderer> renderer = std::make_unique<Renderer>(*ctx);
+
+    EXPECT_EQ(renderer->getSwapchain().getFormat(), VK_FORMAT_B8G8R8A8_SRGB);
 }
